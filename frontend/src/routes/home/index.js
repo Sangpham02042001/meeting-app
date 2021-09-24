e86c0df25a979e3b1072fe335075365905e69569
Vdtrung_ <hellovdtvip@gmail.com>
2021-09-24 15:32:57 +0700

    template gui router

--- frontend/src/routes/home/index.js
+++ frontend/src/routes/home/index.js
@@ -39,21 +39,24 @@ const Home = (props) => {
                             </NavLink>
                         </div>
                         <div className="btn-list-selection">
-                            <NavLink to='/teams'  activeClassName="btn-active">
+                            <NavLink to='/teams' activeClassName="btn-active">
                                 <button className="btn-default"> Group</button>
                             </NavLink>
                         </div>
                         <div className="btn-list-selection">
-                            <NavLink to='/setting'  activeClassName="btn-active">
+                            <NavLink to='/setting' activeClassName="btn-active">
                                 <button className="btn-default" >Setting</button>
                             </NavLink>
                         </div>
                     </div>
                     <Switch>
                         <Route exact path="/">
-                            <h1>
-                                Hello
-                            </h1>
+                            <div className="home-component">
+                                Write here
+                                <h1>
+                                    Hello
+                                </h1>
+                            </div>
                         </Route>
                         <Route path="/profile">
                             <Profile />
