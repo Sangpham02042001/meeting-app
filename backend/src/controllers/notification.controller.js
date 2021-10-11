2ba0169d288cf7dc76066e19abb08bb5e34f10a4
Sang Pham <psang242001@gmail.com>
2021-10-12 00:03:14 +0700

    update notification API

--- backend/src/controllers/notification.controller.js
+++ backend/src/controllers/notification.controller.js
@@ -1,7 +1,44 @@
 const Notification = require('../models/notification')
 
 const updateRead = async (req, res) => {
-
+  let { notiId } = req.params
+  let { id } = req.auth
+  try {
+    let notification = await Notification.findOne({
+      where: {
+        id: notiId
+      }
+    })
+    if (notification.userId != id) {
+      throw `Not authorized`
+    }
+    notification.isRead = true
+    await notification.save()
+    return res.status(200).json({ notification })
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json({ error })
+  }
 }
 
-module.exports = { updateRead }
+const deleteNotification = async (req, res) => {
+  let { notiId } = req.params
+  let { id } = req.auth
+  try {
+    let notification = await Notification.findOne({
+      where: {
+        id: notiId
+      }
+    })
+    if (notification.userId != id) {
+      throw `Not authorized`
+    }
+    await notification.destroy()
+    return res.status(200).json({ message: 'Delete notification successfully' })
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json({ error })
+  }
+}
+
+module.exports = { updateRead, deleteNotification }
--- backend/src/routes/notification.routes.js
+++ backend/src/routes/notification.routes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express')
-const { updateRead } = require('../controllers/notification.controller')
+const { updateRead, deleteNotification } = require('../controllers/notification.controller')
 const { requireSignin } = require('../controllers/auth.controller')
 
 const router = Router()
 
 router.route('/api/notifications/:notiId')
   .put(requireSignin, updateRead)
+  .delete(requireSignin, deleteNotification)
 
 module.exports = router
