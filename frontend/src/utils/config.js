da87e31eb1f79ab6fe4746598ad82c5c3317136e
Vdtrung_ <hellovdtvip@gmail.com>
2021-11-23 18:52:11 +0700

    update

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,7 +1,7 @@
+const awsServer = "https://3.143.208.58"
 const baseURL = "http://localhost:3001";
-// const baseURL = "https://3.134.88.175"
-const janusServer = "https://3.134.88.175/janus-server/janus"
-
-// "homepage": "https://3.134.88.175/public"
+// const baseURL = awsServer
+const janusServer = awsServer + "/janus-server/janus"
+// "homepage": awsServer + "/public"
 
 export { baseURL, janusServer };
