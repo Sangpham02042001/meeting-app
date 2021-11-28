fdd286abf14c60e843c8e4ac1a71ce0ed9af4bec
Tran Dinh Long <longa5k50@gmail.com>
2021-11-28 16:40:24 +0700

    fix src img

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -135,7 +135,7 @@ export default function Navbar() {
     <nav className="navbar">
       <div style={{ display: 'flex' }}>
         <Link to='/home'>
-          <Avatar src='public/meeting-logo.png' style={{
+          <Avatar src='meeting-logo.png' style={{
             width: '40px',
             height: '40px',
           }} />
